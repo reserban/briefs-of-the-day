31b2b4e5140a3ee107277e873f4cfb2d324b38f9
Alexandru (The) Serban <alexandru@theserban.com>
2024-03-07 10:12:59 +0200

    a

--- JS/script.js
+++ JS/script.js
@@ -321,16 +321,12 @@ function startCountdown() {
 }
 
 function updateProfilePicture(dateString) {
-    const seed = generateSeed(dateString) + 5;
+    const seed = generateSeed(dateString) + 5; 
     const pictureIndex = Math.floor(pseudoRandom(seed) * 300) + 1;
     const imageFileName = `Images/Persons/${pictureIndex}p.jpg`;
 
     const imgElement = document.getElementById('profilePicture');
-    const img = new Image();
-    img.onload = function() {
-        imgElement.src = this.src;
-    };
-    img.src = imageFileName;
+    imgElement.src = imageFileName;
 }
 
 function getLogoIndexFromDate(dateString) {
@@ -579,13 +575,32 @@ function generateBriefForToday(dateString) {
     const briefEndingText = generateBriefEnding(seed); 
     document.getElementById('briefEnding').textContent = briefEndingText; 
 
-    updateProfilePicture(dateString); 
-    updateProfileName(dateString); 
-    updateProfileEmail(dateString); 
-    updateCompanyName(dateString); 
-    updateLogoPicture(dateString); 
+    updateProfilePicture(dateString);
+    updateProfileName(dateString);
+    updateProfileEmail(dateString);
+    updateCompanyName(dateString);
+    updateLogoPicture(dateString);
     updateProductNames(dateString);
     updateProductAreas(dateString);
     updateDownloadButton(dateString);
     adjustCountdownVisibility(selectedDate);
+  
+    // Hide the loading animation after the content is updated
+    const loadingAnimation = document.querySelector('.loading-animation');
+    const profileDetails = document.querySelector('.profileDetails');
+    const briefContent = document.querySelector('.briefContent');
+  
+    loadingAnimation.style.display = 'none';
+    profileDetails.style.display = 'block';
+    briefContent.style.display = 'block';
 }
+
+function showLoadingAnimation() {
+    const loadingAnimation = document.querySelector('.loading-animation');
+    const profileDetails = document.querySelector('.profileDetails');
+    const briefContent = document.querySelector('.briefContent');
+  
+    loadingAnimation.style.display = 'block';
+    profileDetails.style.display = 'none';
+    briefContent.style.display = 'none';
+  }
